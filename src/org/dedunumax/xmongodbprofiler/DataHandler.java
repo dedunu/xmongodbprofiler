6392272118078d1875131d95574179f0e8cba6d9
Dedunu Dhananjaya <dedunumax@gmail.com>
2014-02-07 23:26:45 +0530

    Removed obsolete methods

--- src/org/dedunumax/xmongodbprofiler/DataHandler.java
+++ src/org/dedunumax/xmongodbprofiler/DataHandler.java
@@ -1,10 +1,7 @@
 package org.dedunumax.xmongodbprofiler;
 
-import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
 import java.util.List;
 
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
@@ -29,21 +26,4 @@ public class DataHandler {
 	    fileOut.close();
 	}
 	
-	public static void saveData(String fileName, List<BasicDBObject> list)
-			throws IOException {
-		ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(
-				fileName));
-		oos.writeObject(list);
-		oos.flush();
-		oos.close();
-	}
-
-	public static List<BasicDBObject> loadData(String fileName)
-			throws IOException, ClassNotFoundException {
-		ObjectInputStream ois = new ObjectInputStream(new FileInputStream(
-				fileName));
-		List<BasicDBObject> list = (List<BasicDBObject>) ois.readObject();
-		ois.close();
-		return list;
-	}
 }
